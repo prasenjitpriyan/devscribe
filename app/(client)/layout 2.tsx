fd1e15a63e766f32b972fb518bcaf42e73cde6eb
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-01-27 21:04:41 +0530

    Slug added in the posts

---
+++ app/(client)/layout 2.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from 'next'
+import { Fira_Code } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+import { Provider } from '@/utils/Provider'
+
+const firaCode = Fira_Code({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://next-cms-blog-ce.vercel.app/'),
+  title: {
+    default: 'Dev Blook - A blog for developers',
+    template: '%s | Dev Blook - A blog for developers'
+  },
+  description: 'A blog for developers by developers!',
+  openGraph: {
+    title: 'Dev Blook - A blog for developers',
+    description: 'A blog for developers by developers!',
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://next-cms-blog-ce.vercel.app/',
+    siteName: 'DevBlook'
+  }
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${firaCode.className} h-full bg-amber-50 text-indigo-950 dark:bg-slate-950 dark:text-amber-50 dark:selection:bg-purple-500`}
+      >
+        <Provider>
+          <Navbar />
+          <main className="mx-auto max-w-5xl px-6">{children}</main>
+        </Provider>
+      </body>
+    </html>
+  )
+}
---
+++ app/(client)/posts/[slug]/page.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { client } from '@/sanity/lib/client'
+import Header from '@/components/Header'
+import { Post } from '@/utils/interface'
+import Link from 'next/link'
+import { VT323 } from 'next/font/google'
+import { notFound } from 'next/navigation'
+
+interface Params {
+  params: {
+    slug: string
+  }
+}
+
+const dateFont = VT323({ weight: '400', subsets: ['latin'] })
+
+async function getPost(slug: string) {
+  const query = `
+  *[_type == "post" && slug.current == "${slug}"][0] {
+    title,
+    slug,
+    publishedAt,
+    excerpt,
+    _id,
+    "headings": body[style in ["h2", "h3", "h4", "h5", "h6"]],
+    body,
+    tags[]-> {
+      _id,
+      slug,
+      name
+    },
+  }
+  `
+  const post = await client.fetch(query)
+  return post
+}
+
+const Page = async ({ params }: Params) => {
+  const { slug } = await params
+
+  const post: Post = await getPost(slug)
+
+  if (!post) {
+    notFound()
+  }
+
+  return (
+    <div>
+      <Header title={post?.title} />
+      <div className="text-center">
+        <span className={`${dateFont.className} text-purple-500`}>
+          {new Date(post?.publishedAt).toDateString()}
+        </span>
+        <div className="mt-5">
+          {post?.tags?.map((tag) => (
+            <Link key={tag?._id} href={`/tag/${tag.slug.current}`}>
+              <span className="mr-2 p-1 rounded-sm text-sm lowercase dark:bg-gray-950 border dark:border-gray-900">
+                #{tag.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Page
